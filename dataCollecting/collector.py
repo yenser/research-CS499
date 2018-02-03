7a18998ca2cb2b9327150be09343cc8efa8e2ad7
yenser <jens.streck@gmail.com>
2018-02-02 18:47:46 -0600

    collector app

--- dataCollecting/collector.py
+++ dataCollecting/collector.py
@@ -1,29 +1,25 @@
-from pandas_datareader import data
-import pandas as pd
+from alpha_vantage.timeseries import TimeSeries
+import matplotlib.pyplot as plt
 
 
-# Define the instruments to download. We would like to see Apple, Microsoft and the S&P500 index.
-tickers = ['AAPL', 'MSFT', 'SPY']
+ts = TimeSeries(key='ZR07VE377CJYBGLJ',output_format='pandas')
+comp = 'MSFT'
+# Get json object with the intraday data and another with the call's metadata
 
-# Define which online source one should use
-data_source = 'google'
 
-# We would like all available data from 01/01/2000 until 12/31/2016.
-start_date = '2010-01-01'
-end_date = '2016-12-31'
+try:
+	print("getting Data for ", comp, "...")
+	data, meta_data = ts.get_intraday(symbol=comp,interval='60min', outputsize='full')
+	print("Data retrieved for ", comp)
+	try:
+		data.plot()
+		plt.title('Intraday Times Series for the '+comp+' stock (60 min)')
+		plt.show()
+	except:
+		print('data plotting error')
+except:
+	print("Servers are busy... try again in a bit")
 
-# User pandas_reader.data.DataReader to load the desired data. As simple as that.
-panel_data = data.DataReader(tickers, data_source, start_date, end_date)
 
-# Getting just the adjusted closing prices. This will return a Pandas DataFrame
-# The index in this DataFrame is the major index of the panel_data.
-close = panel_data.ix['Close']
 
-# Getting all weekdays between 01/01/2000 and 12/31/2016
-all_weekdays = pd.date_range(start=start_date, end=end_date, freq='B')
 
-# How do we align the existing prices in adj_close with our new set of dates?
-# All we need to do is reindex close using all_weekdays as the new index
-close = close.reindex(all_weekdays)
-
-close.head(10)
