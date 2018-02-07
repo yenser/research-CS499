579cff944f0e3e375f39a5d239691f8152eca237
Jens Streck <yenser@Jens-MacBook-Pro.local>
2018-02-06 21:03:50 -0600

    data creator and my data

--- dataCollecting/makeFiles.py
+++ dataCollecting/makeFiles.py
@@ -5,32 +5,45 @@ from pandas.io.json import json_normalize
 from requests import Session
 
 
+session = Session()
 comp = ''
 apikey = 'ZR07VE377CJYBGLJ'
 
 
 def parse_args():
 	parser = argparse.ArgumentParser(description='Pull Data Program')
-
 	parser.add_argument('company', type=str, nargs=1, help='Company Acronym')
-
 	return parser.parse_args()
 
 def requestMonth(comp):
+	print("getting Data for ", comp, "...")
+	url='https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol='+comp+'&outputsize=full&apikey='+apikey+'&datatype=csv'
+	res = session.get(url)
+	return res.text
 
-	url='https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol='+comp+'&interval=15min&outputsize=compact&apikey='+apikey
-    res = session.get(url)
+def requestWeek(comp):
+	print("getting Data for ", comp, "...")
+	url='https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol='+comp+'&outputsize=full&apikey='+apikey+'&datatype=csv'
+	res = session.get(url)
+	return res.text
 
-    return res.status_code == 200 and 'Login Attempt Failed' not in res.text
+def requestDaily(comp):
+	#not working yet
+	print("getting Data for ", comp, "...")
+	url='https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol='+comp+'&outputsize=full&apikey='+apikey+'&datatype=csv'
+	res = session.get(url)
+	return res.text
+
 
 def manageDirectory(comp):
 	#Check Directory
 	if not os.path.exists('../data/'+comp+'/'):
 		os.makedirs('../data/'+comp+'/')
 
-def manageFile(comp, ext):
+
+
+def manageFile(fname):
 	#Remove fname.json
-	fname = comp+ext
 	if(os.path.isfile(fname)):
 		try:
 			print('Removing previous file: ', fname)
@@ -38,14 +51,18 @@ def manageFile(comp, ext):
 		except Exception as e:
 			print('Removing file ran into an issue: ', e)
 
-def writeFile(comp, ext):
 
-	manageFile(comp, ext)
 
+
+def writeFile(comp, ext, data, length):
+	fname = '../data/'+comp+'/'+length+'_'+comp+ext
+	manageFile(fname)
 	try:
-		text_file = open(fnamePANDAS, "w")
-		text_file.write(str(data))
+		print("Writing file ", fname)
+		text_file = open(fname, "w")
+		text_file.write(data)
 		text_file.close()
+		print('Writing complete')
 	except Exception as e:
 		print("Writing to file failed: ", e)
 
@@ -61,12 +78,16 @@ if __name__ == '__main__':
 	# Get json object with the intraday data and another with the call's metadata
 
 	try:
-		print("getting Data for ", comp, "...")
-		data, meta_data = ts.get_intraday(symbol=comp,interval='15min', outputsize='full')
-		print("Data retrieved for ", comp)
-
+		
 		manageDirectory(comp)
-		writeFile(comp, '.json')
+
+		# get monthly
+		data = requestMonth(comp)
+		writeFile(comp, '.csv', data, 'MONTHLY')
+		
+		# get weekly
+		data = requestWeek(comp)
+		writeFile(comp, '.csv', data, 'WEEKLY')
 
 		
 	except Exception as e: 
