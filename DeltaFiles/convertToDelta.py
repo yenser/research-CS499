87a216fa57e9df34bd4c3aca06f0d7070dadf88c
Joel Schilling <Joel1540@gmail.com>
2018-02-14 21:00:26 -0600

    Delta Files
    The pain is over. It works!

--- DeltaFiles/convertToDelta.py
+++ DeltaFiles/convertToDelta.py
@@ -1,16 +1,18 @@
 import csv
 
-with open('test.csv') as csvfile:
-    readcsv = csv.reader(csvfile, delimiter = ',')
+with open('DAILY_AAPL.csv') as csvfile:
+    with open('DAILY_AAPL_DELTA.csv', 'w') as deltafile:
 
-    csvlist = list(readcsv)
+        readcsv = csv.reader(csvfile, delimiter = ',')
+        writecsv = csv.writer(deltafile, delimiter = ',')
+        csvlist = list(readcsv)
 
-    print (len(csvlist))
+        print (len(csvlist))
 
-    i = 2
+        i = 2
 
-    for i in range(1, len(csvlist)):
-
-        j = i - 1
-        #print(csvlist[1][1])
-        print(i-1, (float(csvlist[j][0]) - float(csvlist[i][0])),)
+        for i in range(2, len(csvlist)):
+            k = i
+            j = k - 1
+            #print(csvlist[1][1])
+            writecsv.writerow([str(float(csvlist[j][1]) - float(csvlist[i][1]))])
